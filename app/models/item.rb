bd624f342740c959ef4a83240a86b16eedd995f0
Wasabi5392 <taka.dd.sdr1729@gmail.com>
2023-01-15 00:01:18 +0900

    dependent: :destroy設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
-  has_one :order
+  has_one :order, dependent: :destroy
+  has_one :address, through: :order, dependent: :destroy
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :status
