1436e9f886616b1d63da4893b5c5c6159c2f31ae
Wasabi5392 <taka.dd.sdr1729@gmail.com>
2023-01-14 23:45:31 +0900

    商品購入機能実装に伴う画像表示などの分岐設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,7 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
+  before_action :non_purchased_item, only: [:index, :create]
+
   def index
     @order_address = OrderAddress.new
   end
@@ -18,5 +20,10 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:order_address).permit(:post_code, :prefecture_id, :city, :house_number, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def non_purchased_item
+    @item = Item.find(params[:item_id])
+    redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
   end
 end
